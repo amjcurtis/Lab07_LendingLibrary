6b54ea1375c4d89b24faf051a17c16f346344352
Andrew Curtis <curtis.andrewm@gmail.com>
2019-03-28 22:25:14 -0700

    Add progress on Borrow method

--- Lab07_LendingLibrary/Classes/Library.cs
+++ Lab07_LendingLibrary/Classes/Library.cs
@@ -5,12 +5,20 @@ using System.Text;
 
 namespace Lab07_LendingLibrary.Classes
 {
-    // Create custom generic collection
-    class Library<T> : IEnumerable
+
+    /// <summary>
+    /// Custom generic collection
+    /// </summary>
+    /// <typeparam name="T">Book type</typeparam>
+    public class Library<T> : IEnumerable
     {
         T[] books = new T[10];
         int count = 0;
 
+        /// <summary>
+        /// Adds a Book object to Library collection
+        /// </summary>
+        /// <param name="book">Book object</param>
         public void Add(T book)
         {
             if (count == books.Length)
@@ -22,34 +30,47 @@ namespace Lab07_LendingLibrary.Classes
             books[count++] = book;
         }
 
+        /// <summary>
+        /// Removes a Book object from Library collection
+        /// </summary>
+        /// <param name="book">Book object</param>
         public void Remove(T book)
         {
-            T[] temp = null;
+            //T[] temp = null;
+            T[] temp = new T[books.Length];
 
-            //TODO Determine size of temp and if it need resizing
+            // Determine size of temp array and if it need resizing
             if (count < (books.Length - books.Length / 2))
             {
                 Array.Resize(ref books, books.Length - (books.Length / 2));
             }
 
-            //TODO Load temp array with items that don't need removed
-            foreach (T item in books)
+            // Load temp array with items that don't need to be removed
+            for (int i = 0; i < books.Length; i++)
             {
-                //if (item title != book title)
-                //{
+                if (!books[i].Equals(book))
+                {
                     // Add to temp array
-                //}
-
-            } 
-
+                    temp[i] = books[i];
+                }
+            }
+            count--;
             books = temp;
         }
 
+        /// <summary>
+        /// Counts number of Books currently in Library
+        /// </summary>
+        /// <returns>Book count</returns>
         public int Count()
         {
             return count;
         }
 
+        /// <summary>
+        /// Implements IEnumerable
+        /// </summary>
+        /// <returns>Book object at each index of array</returns>
         public IEnumerator<T> GetEnumerator()
         {
             for (int i = 0; i < count; i++)
@@ -58,6 +79,10 @@ namespace Lab07_LendingLibrary.Classes
             }
         }
 
+        /// <summary>
+        /// Implements IEnumerable
+        /// </summary>
+        /// <returns>GetEumerator()</returns>
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
--- Lab07_LendingLibrary/Program.cs
+++ Lab07_LendingLibrary/Program.cs
@@ -3,24 +3,81 @@ using Lab07_LendingLibrary.Classes;
 
 namespace Lab07_LendingLibrary
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
+        {
+            Borrow();
+        }
+
+        /// <summary>
+        /// Outputs to the console all books in library and distributes books to other standard generic lists
+        /// </summary>
+        public static void Borrow()
         {
             // Instantiate custom generic collection
             Library<Book> library = new Library<Book>
             {
-                new Book { Title = "C# 7.0 in a Nutshell", Author = new Author("Joseph", "Albahari"), Genre = Book.Genres.Programming },
-                new Book { Title = "Think Python", Author = new Author("Allen", "Downey"), Genre = Book.Genres.Programming },
-                new Book { Title = "The Lord of the Rings", Author = new Author("J.R.R", "Tolkien"), Genre = Book.Genres.Fiction },
-                new Book { Title = "Farmer Giles of Ham", Author = new Author("J.R.R", "Tolkien"), Genre = Book.Genres.Fiction },
-                new Book { Title = "Roverandom", Author = new Author("J.R.R", "Tolkien"), Genre = Book.Genres.Fiction },
-                new Book { Title = "Leaf by Niggle", Author = new Author("J.R.R", "Tolkien"), Genre = Book.Genres.Fiction },
-                new Book { Title = "Moby Dick", Author = new Author("Daniel", "Defoe"), Genre = Book.Genres.Fiction },
-                new Book { Title = "The Hobbit", Author = new Author("J.R.R", "Tolkien"), Genre = Book.Genres.Fiction },
-                new Book { Title = "Tales from the Perilous Realm", Author = new Author("J.R.R", "Tolkien"), Genre = Book.Genres.Fiction },
-                new Book { Title = "The Adventures of Tom Bombadil", Author = new Author("J.R.R", "Tolkien"), Genre = Book.Genres.Fiction },
+                new Book {
+                    Title = "C# 7.0 in a Nutshell",
+                    Author = new Author("Joseph", "Albahari"),
+                    Genre = Book.Genres.Programming
+                },
+                new Book {
+                    Title = "Think Python",
+                    Author = new Author("Allen", "Downey"),
+                    Genre = Book.Genres.Programming
+                },
+                new Book {
+                    Title = "The Lord of the Rings",
+                    Author = new Author("J.R.R", "Tolkien"),
+                    Genre = Book.Genres.Fiction
+                },
+                new Book {
+                    Title = "Farmer Giles of Ham",
+                    Author = new Author("J.R.R", "Tolkien"),
+                    Genre = Book.Genres.Fiction
+                },
+                new Book {
+                    Title = "Roverandom",
+                    Author = new Author("J.R.R", "Tolkien"),
+                    Genre = Book.Genres.Fiction
+                },
+                new Book {
+                    Title = "Leaf by Niggle",
+                    Author = new Author("J.R.R", "Tolkien"),
+                    Genre = Book.Genres.Fiction
+                },
+                new Book {
+                    Title = "Moby Dick",
+                    Author = new Author("Daniel", "Defoe"),
+                    Genre = Book.Genres.Fiction
+                },
+                new Book {
+                    Title = "The Hobbit",
+                    Author = new Author("J.R.R", "Tolkien"),
+                    Genre = Book.Genres.Fiction
+                },
+                new Book {
+                    Title = "The Book of Lost Tales",
+                    Author = new Author("J.R.R", "Tolkien"),
+                    Genre = Book.Genres.History
+                },
+                new Book {
+                    Title = "The Adventures of Tom Bombadil",
+                    Author = new Author("J.R.R", "Tolkien"),
+                    Genre = Book.Genres.Fiction
+                },
             };
+
+            Console.WriteLine(library.Count());
+
+            Book bookToAdd = new Book { Title = "My New Book", Author = new Author("Anony", "Mous"), Genre = Book.Genres.History };
+            library.Add(bookToAdd);
+
+            Console.WriteLine(library.Count());
+
+            library.Remove(bookToAdd);
 
             foreach (Book book in library)
             {
@@ -29,11 +86,9 @@ namespace Lab07_LendingLibrary
                 Console.WriteLine($"Genre: {book.Genre}");
                 Console.WriteLine("");
             }
+
+
+
         }
-
-
-
-
-
     }
 }
